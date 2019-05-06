6dfda5d5d370c854cd16a108bbfb5f30cc9a4513
isx47983457 <ericescriba98@gmail.com>
2019-05-06 12:12:15 +0200

    new

---
+++ kerberos/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cp /opt/docker/krb5.conf /etc/krb5.conf
+cp /opt/docker/kdc.conf /var/kerberos/krb5kdc/kdc.conf
+cp /opt/docker/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
+
+kdb5_util create -s -P masterkey
+kadmin.local -q "addprinc -pw kpere pere"
+kadmin.local -q "addprinc -pw kpere pere/admin"
+kadmin.local -q "addprinc -pw kanna anna"
+kadmin.local -q "addprinc -pw kpau pau"
+kadmin.local -q "addprinc -pw kmarta marta"
+kadmin.local -q "addprinc -pw kadmin admin"
+
+
