5a73cd7463d404bfa4cfa50efa1dc07c8fa95149
skeret <root@localhost.localdomain>
2019-05-21 19:06:43 +0200

    new

---
+++ push.sh
@@ -0,0 +1,7 @@
+docker push eescriba/ldapserver:greload
+docker push eescriba/kerberos:greload
+docker push eescriba/userhomes:greload
+docker push eescriba/sambaserver:greload
+docker push eescriba/nfsserver:greload
+docker push eescriba/sambahost:greload
+docker push eescriba/nfshost:greload
