5fcb201874b3d07825c294822b1d6a7af05ae995
isx47983457 <ericescriba98@gmail.com>
2019-05-21 10:46:46 +0200

    new

---
+++ pull.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+docker pull eescriba/ldapserver:greload
+docker pull eescriba/kerberos:greload
+docker pull eescriba/userhomes:greload
+docker pull eescriba/sambaserver:greload
+docker pull eescriba/nfsserver:greload
+docker pull eescriba/sambahost:greload
+docker pull eescriba/nfshost:greload
